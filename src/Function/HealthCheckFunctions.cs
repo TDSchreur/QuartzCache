1bf100396f3127e81f6e3713256fade4e508cb29
Dennis Schreur <dennis@tdschreur.nl>
2025-03-17 15:37:14 +0100

    Add OpenAPI support and update HealthCheck and HttpTrekker functions for enhanced API documentation

--- src/Function/HealthCheckFunctions.cs
+++ src/Function/HealthCheckFunctions.cs
@@ -1,6 +1,9 @@
+using System.Net;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace TDS.QuartzCache.Function;
@@ -8,10 +11,15 @@ namespace TDS.QuartzCache.Function;
 public class HealthCheckFunctions(HealthCheckService healthCheck)
 {
     [Function("Health")]
+    [OpenApiOperation(operationId: "Health", tags: ["Health"])]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: MediaTypeNames.Application.Json, bodyType: typeof(HealthReport), Description = "The OK response")]
+    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.ServiceUnavailable, Description = "The service is unavailable")]
     public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         var report = await healthCheck.CheckHealthAsync().ConfigureAwait(false);
 
-        return report.Status == HealthStatus.Unhealthy ? new StatusCodeResult(StatusCodes.Status503ServiceUnavailable) : new OkObjectResult(report);
+        return report.Status == HealthStatus.Unhealthy
+            ? new StatusCodeResult(StatusCodes.Status503ServiceUnavailable)
+            : new OkObjectResult(report);
     }
 }
--- src/Function/HttpTrekker.cs
+++ src/Function/HttpTrekker.cs
@@ -1,6 +1,9 @@
+using System.Net;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Logging;
 using TDS.QuartzCache.CertificateCache;
 
@@ -8,12 +11,19 @@ namespace TDS.QuartzCache.Function;
 
 public class HttpTrekker(ICertificateProvider certificateProvider, ILogger<HttpTrekker> logger)
 {
-    [Function("HttpTrekker")]
+    [Function(nameof(HttpTrekker))]
+    [OpenApiOperation(operationId: nameof(HttpTrekker), tags: ["Tests"])]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: MediaTypeNames.Application.Json, bodyType: typeof(CertResponse), Description = "The OK response")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         logger.LogInformation("C# HTTP trigger function processed a request.");
 
         var cert = certificateProvider.GetCertificate();
-        return new OkObjectResult(cert);
+        return new OkObjectResult(new CertResponse { Cert = cert });
+    }
+
+    public class CertResponse
+    {
+        public string Cert { get; init; }
     }
 }
