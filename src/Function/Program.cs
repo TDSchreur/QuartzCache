7cfcfb44929d5f9456996896947b9258d85cde55
Dennis Schreur <dennis@tdschreur.nl>
2025-03-19 15:22:12 +0100

    Enhance logging configuration in Azure Functions to manage log levels and improve clarity

--- src/Function/Program.cs
+++ src/Function/Program.cs
@@ -19,19 +19,25 @@ internal static class Program
                                         services.AddHealthChecks();
                                     })
                                     // Deze moet als laatste
-                                    // .ConfigureLogging(logging =>
-                                    // {
-                                    //     // // https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=hostbuilder%2Cwindows#managing-log-levels
-                                    //     // // The Application Insights SDK adds a logging filter that instructs the logger to capture only warnings and more severe logs. If you want to disable this behavior, remove the filter rule as part of service configuration:
-                                    //     // logging.Services.Configure<LoggerFilterOptions>(options =>
-                                    //     // {
-                                    //     //     var defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
-                                    //     //     if (defaultRule is not null)
-                                    //     //     {
-                                    //     //         options.Rules.Remove(defaultRule);
-                                    //     //     }
-                                    //     // });
-                                    // })
+                                    .ConfigureLogging(logging =>
+                                    {
+                                        // https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=hostbuilder%2Cwindows#managing-log-levels
+                                        // The Application Insights SDK adds a logging filter that instructs the logger to capture only warnings and more severe logs. If you want to disable this behavior, remove the filter rule as part of service configuration:
+                                        logging.Services.Configure<LoggerFilterOptions>(options =>
+                                        {
+                                            var providerName = typeof(Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider).FullName;
+                                            var defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName == providerName); //// "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+                                            if (defaultRule is not null)
+                                            {
+                                                options.Rules.Remove(defaultRule);
+                                            }
+
+                                            options.MinLevel = LogLevel.Warning;
+                                            options.Rules.Add(new LoggerFilterRule(providerName, "Microsoft", LogLevel.Warning, null));
+                                            options.Rules.Add(new LoggerFilterRule(providerName, "System", LogLevel.Warning, null));
+                                            options.Rules.Add(new LoggerFilterRule(providerName, "TDS", LogLevel.Information, null));
+                                        });
+                                    })
                                     .Build();
 
         host.Run();
