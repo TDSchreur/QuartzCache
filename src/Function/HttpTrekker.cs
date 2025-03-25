0650eb8362fe07ad29cd8e6ca3ab3474d7a09e6a
Dennis Schreur <dennis@tdschreur.nl>
2025-03-25 14:48:05 +0100

    Update Microsoft.Azure.Functions.Worker.Extensions.EventGrid to version 3.5.0; remove obsolete HttpCatchAll function and adjust HttpTrekker route

--- src/Function/HttpTrekker.cs
+++ src/Function/HttpTrekker.cs
@@ -11,10 +11,18 @@ namespace TDS.QuartzCache.Function;
 
 public class HttpTrekker(ICertificateProvider certificateProvider, ILogger<HttpTrekker> logger)
 {
+    //// [Function(nameof(HttpCatchAll))]
+    //// [OpenApiOperation(operationId: nameof(HttpCatchAll), tags: ["Tests"])]
+    //// [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: MediaTypeNames.Text.Plain, bodyType: typeof(CertResponse), Description = "The OK response")]
+    //// public static IActionResult HttpCatchAll([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "{*catch-all}")] HttpRequest req)
+    //// {
+    ////     return new OkObjectResult(new { message = $"Hello from catch-all route! request: {req.Path}" });
+    //// }
+
     [Function(nameof(HttpTrekker))]
     [OpenApiOperation(operationId: nameof(HttpTrekker), tags: ["Tests"])]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: MediaTypeNames.Application.Json, bodyType: typeof(CertResponse), Description = "The OK response")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "httptrekker")] HttpRequest req)
     {
         logger.LogInformation("C# HTTP trigger function processed a request.");
 
