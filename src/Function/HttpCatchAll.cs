3e05cb7389ed1db83424753f500c64f6bc815212
Dennis Schreur <dennis@tdschreur.nl>
2025-03-25 14:22:41 +0100

    Refactor HttpCatchAll function to use IActionResult and simplify response handling

--- src/Function/HttpCatchAll.cs
+++ src/Function/HttpCatchAll.cs
@@ -1,16 +1,14 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 
 namespace TDS.QuartzCache.Function;
 
 public static class HttpCatchAll
 {
     [Function(nameof(HttpCatchAll))]
-    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "{*catch-all}")] HttpRequestData req)
+    public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "{*catch-all}")] HttpRequest req)
     {
-        var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-        response.WriteString($"Hello from catch-all route! request: {req.Url}");
-        return response;
+        return new OkObjectResult(new { message = $"Hello from catch-all route! request: {req.Path}" });
     }
 }
